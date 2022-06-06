7bf50c54ce563dd024824bbf8834447671b18228
Artem <artem.ysanov2217@gmail.com>
2022-06-06 16:03:38 +0300

    laba18

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите х : ");
+        int x = scanner.nextInt();
+        System.out.println("Введите у : ");
+        int y = scanner.nextInt();
+        Formula formula = Formula.getFormula();
+        formula.setFormula(x, y);
+        formula.displayFormula();
+        Formula formula1 = Formula.getFormula();
+        if (formula.hashCode() == formula1.hashCode()){
+            System.out.println("Okey");
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
+class Formula{
+    private static Formula formula = new Formula();
+    private static Formula Formula;
+    private int x;
+    private int y;
+    private Formula(){
+
+    }
+    public static Formula getFormula(){
+        return formula;
+    }
+    public void setFormula(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public static Formula getInstance(){
+        return Formula;
+    }
+    public void displayFormula(){
+        double vector = 2 * x + 3 / y;
+        System.out.println(vector);
+    }
+}
